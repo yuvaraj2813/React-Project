5924e83f69ecfc8e78675f672e5f8bb52540c874
Yuvaraj T <144918017+yuvaraj2813@users.noreply.github.com>
2025-04-30 06:28:38 +0530

    Update previousDonations.jsx

--- front-end/src/doner/Pages/donate/previousDonations.jsx
+++ front-end/src/doner/Pages/donate/previousDonations.jsx
@@ -271,7 +271,7 @@ const PreviousDonations = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">Previous Donations</h1>
+      <h1 className="text-3xl font-bold">Shared Foods</h1>
       <p className="text-gray-500 mb-4">
         Here are your previous donations that were completed successfully.
       </p>
